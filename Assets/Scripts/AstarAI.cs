e7590d37e54272907b08831c724a2cca71530c5e
taixihuase <taixihuase@hotmail.com>
2017-10-12 15:39:45 +0800

    Add Grid Graph

---
+++ Assets/Scripts/AstarAI.cs
@@ -0,0 +1,70 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstarAI : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 currentTargetPos;
+
+    private Seeker seeker;
+
+    private Path path;
+
+    private CharacterController controller;
+
+    private int currentWaypointIndex;
+    
+    [SerializeField]
+    private float startNextWaypointDistance;
+
+    [SerializeField]
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		if(target)
+        {
+            currentTargetPos = target.position;
+        }
+        seeker = GetComponent<Seeker>();
+        if(seeker)
+        {
+            seeker.StartPath(transform.position, currentTargetPos, OnPathComplete);
+        }
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target && target.position != currentTargetPos)
+        {
+            currentTargetPos = target.position;
+            if(seeker && seeker.IsDone())
+            {
+                seeker.StartPath(transform.position, currentTargetPos, OnPathComplete);
+            }
+        }
+
+        if(path != null && controller && currentWaypointIndex < path.vectorPath.Count)
+        {
+            Vector3 dir = (path.vectorPath[currentWaypointIndex] - transform.position).normalized;
+            controller.SimpleMove(dir * moveSpeed);
+            if ((transform.position - path.vectorPath[currentWaypointIndex]).sqrMagnitude <= startNextWaypointDistance * startNextWaypointDistance)
+            {
+                currentWaypointIndex++;
+            }
+        }
+	}
+
+    private void OnPathComplete(Path path)
+    {
+        if(!path.error)
+        {
+            this.path = path;
+            currentWaypointIndex = 0;
+        }
+    }
+}
